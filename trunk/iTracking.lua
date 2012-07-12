8784771e1ffc3a7d719696f419576d11aff3bc66
grdn <grdn@6bbd1610-3471-4d0f-81ad-a635d5cf30b2>
2012-07-12 08:43:10 +0000

    - Removed AceAddon-3.0 from the mod

--- trunk/iTracking.lua
+++ trunk/iTracking.lua
@@ -2,37 +2,44 @@
 -- Setting up scope and libs
 -----------------------------------
 
-local AddonName = select(1, ...);
-iTracking = LibStub("AceAddon-3.0"):NewAddon(AddonName, "AceEvent-3.0");
+local AddonName, iTracking = ...;
+LibStub("AceEvent-3.0"):Embed(iTracking);
+
+local _G = _G;
 
 -----------------------------
--- Setting up the feed
+-- Setting up the LDB
 -----------------------------
 
-iTracking.Feed = LibStub("LibDataBroker-1.1"):NewDataObject(AddonName, {
+iTracking.ldb = LibStub("LibDataBroker-1.1"):NewDataObject(AddonName, {
 	type = "data source",
 	text = "",
 });
 
-function iTracking.Feed.OnClick(anchor, button)
-	if( button == "RightButton" ) then
+iTracking.ldb.OnClick = function(anchor, button)
+	if( button == "RightButton" and not _G.IsModifierKeyDown() ) then
 		_G.ToggleDropDownMenu(1, nil, _G.MiniMapTrackingDropDown, anchor, 0, 0);
 	end
 end
+
+iTracking.ldb.OnEnter = function() end
+iTracking.ldb.OnLeave = iTracking.ldb.OnEnter;
 
 ----------------------
 -- Initializing
 ----------------------
 
-function iTracking:OnInitialize()
+function iTracking:Boot()
 	self:RegisterEvent("PLAYER_ALIVE", "TrackUpdate");
 	self:RegisterEvent("MINIMAP_UPDATE_TRACKING", "TrackUpdate");
-	self:RegisterEvent("PLAYER_ENTERING_WORLD", "TrackUpdate");
 	
 	_G.MiniMapTracking:UnregisterAllEvents();
 	_G.MiniMapTracking.Show = _G.MiniMapTracking.Hide;
 	_G.MiniMapTracking:Hide();
+	
+	self:TrackUpdate();
 end
+iTracking:RegisterEvent("PLAYER_ENTERING_WORLD", "Boot");
 
 ------------------------------------------
 -- Update
@@ -52,13 +59,13 @@ function iTracking:TrackUpdate()
 			tracking = tracking + 1;
 			
 			if( tracking == 1 ) then
-				self.Feed.icon = icon;
+				self.ldb.icon = icon;
 			end
 		end
 	end
 	
 	if( tracking == 0 ) then
-		self.Feed.icon = "Interface\\Minimap\\Tracking\\None";
+		self.ldb.icon = "Interface\\Minimap\\Tracking\\None";
 	end
-	self.Feed.text = tracking.."/"..numTrackings;
+	self.ldb.text = tracking.."/"..numTrackings;
 end
